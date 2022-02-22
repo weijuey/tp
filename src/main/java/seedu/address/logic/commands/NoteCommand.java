00ac47930df2c875bfb4ac2e3dfcb040ef258b14
weijuey <e0543600@u.nus.edu>
2022-02-22 14:02:07 +0800

    Add non-functional skeleton of NoteCommand

---
+++ src/main/java/seedu/address/logic/commands/NoteCommand.java
@@ -0,0 +1,59 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+/**
+ * Updates the notes of a person
+ */
+public class NoteCommand extends Command {
+    public static final String COMMAND_WORD = "note";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits the remark of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing remark will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "r/ [REMARK]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "r/ Likes to swim.";
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "Remark command not implemented yet";
+
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Note: %2$s";
+
+    private final Index index;
+    private final String note;
+
+    public NoteCommand(Index index, String note) {
+        requireAllNonNull(index, note);
+
+        this.index = index;
+        this.note = note;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS,
+                index.getOneBased(), note));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof NoteCommand)) {
+            return false;
+        }
+
+        NoteCommand e = (NoteCommand) other;
+        return index.equals(e.index)
+                && note.equals(e.note);
+    }
+}
