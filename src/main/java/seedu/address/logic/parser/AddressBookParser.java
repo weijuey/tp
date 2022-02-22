318d642e1e7d27aee15bfc885300bb52bc5a33c4
weijuey <e0543600@u.nus.edu>
2022-02-22 14:17:34 +0800

    Update parsers to be able to recognise and parse a note command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -70,7 +70,7 @@ public class AddressBookParser {
             return new HelpCommand();
 
         case NoteCommand.COMMAND_WORD:
-            return new NoteCommand();
+            return new NoteCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
