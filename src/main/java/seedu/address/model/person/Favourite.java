09b6cbf8a695498db793f0fac8828c94ac7db60b
Ezekiel <takufunkai@gmail.com>
2022-03-07 17:33:35 +0800

    Add favourite to storage

--- src/main/java/seedu/address/model/person/Favourite.java
+++ src/main/java/seedu/address/model/person/Favourite.java
@@ -4,19 +4,46 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 public class Favourite {
+    public static final Favourite IS_FAVOURITE = new Favourite(true);
+    public static final Favourite NOT_FAVOURITE = new Favourite(false);
+    public static final String MESSAGE_CONSTRAINTS =
+            "Favourite should be either true or false, and should not be blank.";
 
-    private final Boolean isFavourite;
     public final String value;
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
 
     /**
      * Constructs a {@code Phone}.
      *
      * @param isFavourite The favourite status.
      */
-    public Favourite(Boolean isFavourite) {
+    private Favourite(Boolean isFavourite) {
         requireNonNull(isFavourite);
-        this.isFavourite = isFavourite;
         value = isFavourite.toString();
+    }
+
+    public static Favourite valueOf(String isFavourite) {
+        return "true".equals(isFavourite)
+                ? IS_FAVOURITE
+                : NOT_FAVOURITE;
+    }
+
+    /**
+     * Returns true if a given string is a valid favourite status.
+     */
+    public static boolean isValidFavourite(String test) {
+        return test.matches(VALIDATION_REGEX)
+                && ("true".equals(test) || "false".equals(test));
+    }
+
+
+    public boolean isFavourite() {
+        return this.equals(IS_FAVOURITE);
     }
 
     @Override
