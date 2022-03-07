b28ab5022de6aa2a86316e87590e31bcf8aa3ec6
Ezekiel <takufunkai@gmail.com>
2022-03-07 18:05:32 +0800

    Finalize favourite functionality

--- src/main/java/seedu/address/logic/commands/FavouriteCommand.java
+++ src/main/java/seedu/address/logic/commands/FavouriteCommand.java
@@ -1,29 +1,75 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Favourite;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Tag;
+
+import java.util.List;
+import java.util.Set;
 
 public class FavouriteCommand extends Command {
     public static final String COMMAND_WORD = "fav";
-    public static final String MESSAGE_ARGUMENTS = "Index: %1$d";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Favourites the person identified by the index number used in the displayed person list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
+    public static final String MESSAGE_FAVOURITE_PERSON_SUCCESS = "Changed Person's Favourite Status: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
 
-    private final Index index;
+    private final Index targetIndex;
 
-    public FavouriteCommand(Index index) {
-       requireNonNull(index);
+    public FavouriteCommand(Index targetIndex) {
+       requireNonNull(targetIndex);
 
-       this.index = index;
+       this.targetIndex = targetIndex;
     }
 
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from favourites");
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Person personToFavourite = lastShownList.get(targetIndex.getZeroBased());
+        Favourite newFavouriteStatus = personToFavourite.getFavouriteStatus().equals(Favourite.IS_FAVOURITE)
+                ? Favourite.NOT_FAVOURITE
+                : Favourite.IS_FAVOURITE;
+        Person editedPerson = createFavouritedPerson(personToFavourite, newFavouriteStatus);
+
+        if (!personToFavourite.isSamePerson(editedPerson) && model.hasPerson(editedPerson)) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        }
+
+        model.setPerson(personToFavourite, editedPerson);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(String.format(MESSAGE_FAVOURITE_PERSON_SUCCESS, editedPerson));
+    }
+    
+    private static Person createFavouritedPerson(Person personToEdit, Favourite newFavouriteStatus) {
+        assert personToEdit != null;
+        assert newFavouriteStatus != null;
+
+        Name name = personToEdit.getName();
+        Phone phone = personToEdit.getPhone();
+        Email email = personToEdit.getEmail();
+        Address address = personToEdit.getAddress();
+        Set<Tag> tags = personToEdit.getTags();
+
+        return new Person(name, phone, email, address, tags, newFavouriteStatus);
     }
 
     @Override
@@ -37,6 +83,6 @@ public class FavouriteCommand extends Command {
         }
 
         FavouriteCommand e = (FavouriteCommand) other;
-        return index.equals(e.index);
+        return targetIndex.equals(e.targetIndex);
     }
 }
