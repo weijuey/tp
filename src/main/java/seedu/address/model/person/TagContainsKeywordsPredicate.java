dd7c0dfd45c11141ef9630ad3ed764b7f12087b6
tehkokhoe <tkhoe210@hotmail.com>
2022-03-10 10:18:10 +0800

    no message

---
+++ src/main/java/seedu/address/model/person/TagContainsKeywordsPredicate.java
@@ -0,0 +1,38 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code Person}'s {@code Tag} matches any of the keywords given.
+ */
+public class TagContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public TagContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        boolean hasAllMatches = true;
+
+        for (String keyword : keywords) {
+            boolean doesKeywordMatchAnyTags = person.getTags().stream()
+                    .anyMatch(tag -> StringUtil.containsWordIgnoreCase(tag.tagName, keyword));
+            hasAllMatches = hasAllMatches && doesKeywordMatchAnyTags;
+        }
+
+        return hasAllMatches;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TagContainsKeywordsPredicate // instanceof handles nulls
+                && keywords.equals(((TagContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
