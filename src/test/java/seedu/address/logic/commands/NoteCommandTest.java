d4463c266688a482bd4fc86bf5e2a47f0ea257c6
Yoong Wei Jue <e0543600@u.nus.edu>
2022-03-13 14:21:02 +0800

    Update note command tests

--- src/test/java/seedu/address/logic/commands/NoteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/NoteCommandTest.java
@@ -5,9 +5,11 @@ import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Note;
+import seedu.address.model.person.Notes;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
+
+import java.util.List;
 
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
@@ -16,18 +18,30 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 class NoteCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private String VALID_NOTE = "Bestie";
+    private String INVALID_NOTE = "    ";
+    private Notes VALID_NOTES = Notes.loadNotesFromList(List.of(VALID_NOTE));
 
     @Test
-    void execute_addNoteUnfilteredList_success() {
-        NoteCommand noteCommand = new NoteCommand(INDEX_FIRST_PERSON, new Note(VALID_NOTE));
+    void execute_addValidNote_success() {
+        NoteCommand noteCommand = new NoteCommand(INDEX_FIRST_PERSON, VALID_NOTE);
 
         Person personToEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        Person editedPerson = new PersonBuilder(personToEdit).withNote(VALID_NOTE).build();
+        Person editedPerson = new PersonBuilder(personToEdit).withNotes(VALID_NOTES).build();
         AddressBook newAddressBook = getTypicalAddressBook();
         newAddressBook.setPerson(personToEdit, editedPerson);
         String expectedMessage = String.format(NoteCommand.MESSAGE_UPDATE_NOTE_SUCCESS, editedPerson);
         Model expectedModel = new ModelManager(newAddressBook, new UserPrefs());
 
         assertCommandSuccess(noteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    void execute_addInvalidNote_noChange() {
+        NoteCommand noteCommand = new NoteCommand(INDEX_FIRST_PERSON, INVALID_NOTE);
+
+        Person personToEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        String expectedMessage = String.format(NoteCommand.MESSAGE_NO_UPDATE_TO_NOTES, personToEdit);
+
+        assertCommandSuccess(noteCommand, model, expectedMessage, model);
     }
 }
