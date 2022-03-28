d792f13eed7223151bd197955c75c1d58594eb3b
glennljw <glennljw7@gmail.com>
2022-03-28 22:41:45 +0800

    Remove commented out code

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -201,23 +201,6 @@ public class EditCommand extends Command {
             return Optional.ofNullable(deadlines);
         }
 
-        ///**
-        // * Sets {@code tags} to this object's {@code tags}.
-        // * A defensive copy of {@code tags} is used internally.
-        // */
-        //public void setTags(Set<Tag> tags) {
-        //    this.tags = (tags != null) ? new HashSet<>(tags) : null;
-        //}
-        //
-        ///**
-        // * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
-        // * if modification is attempted.
-        // * Returns {@code Optional#empty()} if {@code tags} is null.
-        // */
-        //public Optional<Set<Tag>> getTags() {
-        //    return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
-        //}
-
         @Override
         public boolean equals(Object other) {
             // short circuit if same object
