0227ed8304c26f6438991812db1d96af08b9d51d
tehkokhoe <tkhoe210@hotmail.com>
2022-03-29 15:59:51 +0800

    Add message for sorting list

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,5 +10,6 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_INVALID_CRITERIA = "%1$s is an invalid criteria!";
+    public static final String MESSAGE_SORTED = "List sorted by %1$s";
 
 }
